6e39aadf6a7de6ee9fe7faf981edb5246345336b
Satya <satya@Satyas-MacBook-Air.local>
2024-02-26 16:26:13 +0530

    Pattern 3 : Lazy initialization

--- Singleton Design Pattern/ChocateFactory/src/Main.java
+++ Singleton Design Pattern/ChocateFactory/src/Main.java
@@ -1,7 +1,14 @@
+import singletonDesignPattern.pattern3.ChocolateBoilerPattern3;
+
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        ChocolateBoilerPattern3 instance1 = ChocolateBoilerPattern3.getInstance();
+        ChocolateBoilerPattern3 instance2 = ChocolateBoilerPattern3.getInstance();
 
+        System.out.println(instance1.hashCode());
+        System.out.println(instance2.hashCode());
     }
 }
